33ade1e5c3119589300662b8fd432792d70b421f
qianfeng <qianfeng@lijian.local>
2014-09-26 09:13:35 +0800

    Initial Commit

---
+++ 绘图/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  绘图
+//
+//  Created by qianfeng on 14-9-26.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 绘图/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  绘图
+//
+//  Created by qianfeng on 14-9-26.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
