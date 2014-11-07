7665f275ebc0975c08ca2919e4b9b8143ffea792
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2014-11-07 17:58:51 +0800

    测试
    测试

--- 绘图/AppDelegate.m
+++ 绘图/AppDelegate.m
@@ -13,6 +13,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"1233");
+    NSLog(@"uwgfoalgf");
+    NSLog(@"1233");
+    NSLog(@"uwgfoalgf");
     return YES;
 }
 							
